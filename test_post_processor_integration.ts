6ced1870252c8ed92be49e3c2acbc8f7ef2b8347
htafolla <htafolla@gmail.com>
2026-01-21 21:26:24 -0600

    test: add file to verify post-processor integration

---
+++ test_post_processor_integration.ts
@@ -0,0 +1,8 @@
+
+// Test file to verify post-processor integration
+export class PostProcessorTest {
+  testMethod() {
+    return 'This should trigger tests-required and documentation-required rules';
+  }
+}
+
